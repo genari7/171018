a05bd8384eb904f2b5a62c5c8a27352d26376ea5
pjh <pjh@localhost>
2017-10-16 13:57:20 +0900

    hotfix ㄲ싱

--- Test02/src/Test02.java
+++ Test02/src/Test02.java
@@ -4,6 +4,7 @@ public class Test02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Test02");
+		System.out.println("Hello hotfix xxxxxxxx");
 	}
 
 }
